e9049b9a3b67ce136bbf944db9ce18a4000bdc0d
Brandan Lennox <brandan@bclennox.com>
2013-01-26 15:14:34 -0500

    Seeds for games and responses.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,10 @@
 require 'yaml'
 
-Player.delete_all
-YAML.load_file(File.join(File.dirname(__FILE__), 'seeds', 'players.yml')).each_pair do |key, player|
-  Player.create!(player)
+%w{player game response}.each do |model|
+  klass = model.to_s.classify.constantize
+
+  YAML.load_file(File.join(File.dirname(__FILE__), 'seeds', "#{model}s.yml")).each_pair do |key, attributes|
+    klass.delete_all
+    klass.create!(attributes)
+  end
 end
