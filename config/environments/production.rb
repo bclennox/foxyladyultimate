3d2b3f26614783d25e6308ca0e359c7af912f6b8
Brandan Lennox <brandan@bclennox.com>
2013-01-29 16:41:49 -0500

    Adding SendGrid config.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,6 +51,16 @@ Ultimate::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com'
+  }
+
   # Enable threaded mode
   # config.threadsafe!
 
