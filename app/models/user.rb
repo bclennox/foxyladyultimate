4b317c22d348addaf9f8ccecd52f67e5e0ca5047
Brandan Lennox <brandan@bclennox.com>
2013-01-31 18:07:55 -0500

    Adding basic devise views.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :recoverable, :timeoutable
   attr_accessible :email, :password, :password_confirmation, :username
+
+  def name
+    username.capitalize
+  end
 end
