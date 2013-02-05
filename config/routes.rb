f57c980d9e5d32bb2af32f7286d2e584e929f8bc
Brandan Lennox <brandan@bclennox.com>
2013-02-04 22:04:51 -0500

    Fixing a Devise deprecation warning.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Ultimate::Application.routes.draw do
-  devise_for :users, skip: :registrations do
-    as :user do
-      get 'users/edit' => 'devise/registrations#edit', as: 'edit_user_registration'
-      put 'users' => 'devise/registrations#update', as: 'user_registration'
-    end
+  devise_for :users, skip: :registrations
+  devise_scope :user do
+    get 'users/edit' => 'devise/registrations#edit', as: 'edit_user_registration'
+    put 'users' => 'devise/registrations#update', as: 'user_registration'
   end
 
   resources :players, except: :show
