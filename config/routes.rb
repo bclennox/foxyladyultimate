299a702bfbe47c068f659849e10fd7d59383d859
Brandan Lennox <brandan@bclennox.com>
2013-02-04 20:09:10 -0500

    Adding change password function.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Ultimate::Application.routes.draw do
-  devise_for :users
+  devise_for :users, skip: :registrations do
+    as :user do
+      get 'users/edit' => 'devise/registrations#edit', as: 'edit_user_registration'
+      put 'users' => 'devise/registrations#update', as: 'user_registration'
+    end
+  end
 
   resources :players, except: :show
   resource :schedule, only: [:edit, :update]
