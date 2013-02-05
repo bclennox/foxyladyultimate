601d48ec214ea1d8530a7078d8749ec85c56a67f
Brandan Lennox <brandan@bclennox.com>
2013-02-04 22:47:40 -0500

    Adding funny messages and working on the e-mail templates.

--- app/mailers/game_mailer.rb
+++ app/mailers/game_mailer.rb
@@ -1,34 +1,46 @@
+# encoding: utf-8
 class GameMailer < ActionMailer::Base
   default from: 'Diana Jarosiewicz <jarosiewicz_diana@cat.com>'
   layout 'game_mailer'
 
   def reminder(game, player, message)
-    @game = game
-    @player = player
-    @message = message
-
-    mail(to: @player.email, subject: "Ultimate Frisbee on #{game_date(@game)}")
+    perform(game, player, message, "Ultimate Frisbee on #{game_date(game)}")
   end
 
   def cancellation(game, player, message)
-    @game = game
-    @player = player
-    @message = message
-
-    mail(to: @player.email, subject: "Canceled: Ultimate Frisbee on #{game_date(@game)}")
+    perform(game, player, message, "Canceled: Ultimate Frisbee on #{game_date(game)}")
   end
 
   def reschedule(game, player, message)
-    @game = game
-    @player = player
-    @message = message
-
-    mail(to: @player.email, subject: "Rescheduled: Ultimate Frisbee on #{game_date(@game)}")
+    perform(game, player, message, "Rescheduled: Ultimate Frisbee on #{game_date(game)}")
   end
 
 private
 
   def game_date(game)
     game.starts_at.strftime('%b %-d')
+  end
+
+  def responses
+    [
+      [ %{Count me in!}, %{Count me out} ],
+      [ %{I'm in!}, %{I'm out} ],
+      [ %{I'm ready to get sweaty!}, %{I have to wash my hair} ],
+      [ %{I want to get skinny!}, %{I'd rather eat a milkshake} ],
+      [ %{I wouldn't miss it!}, %{No thanks, I heard Troy’s coming} ],
+      [ %{I'll be there with bells on!}, %{Maybe next week} ],
+      [ %{I can't wait!}, %{I'm recovering from a volleyball tournament}],
+    ]
+  end
+
+  def perform(game, player, message, subject)
+    to = 'brandan@localhost'
+
+    @game = game
+    @player = player
+    @message = message
+    @positive_response, @negative_response = responses.sample
+
+    mail(to: to, subject: subject)
   end
 end
