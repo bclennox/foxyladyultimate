3342bdea56d491febed93e6939057d795d6a5bb7
Brandan Lennox <brandan@bclennox.com>
2013-02-21 23:32:52 -0500

    Adding support for canceled games.

--- app/services/event_service.rb
+++ app/services/event_service.rb
@@ -1,16 +1,20 @@
 module EventService
   def self.create_event(game)
-    url = game_url(game)
+    url = url(game)
+    sequence = sequence(game)
 
     Icalendar::Calendar.new.tap do |calendar|
+      calendar.ip_method = game.canceled? ? 'CANCEL' : 'PUBLISH'
       calendar.event do
         summary       'Ultimate Frisbee'
         start         game.starts_at.to_datetime
         duration      'PT3H'
         location      game.location
         transparency  'OPAQUE'
-        uid           url
         url           url
+        uid           url
+        sequence      sequence
+        status        game.canceled? ? 'CANCELLED' : 'TENTATIVE'
       end
     end
   end
@@ -18,7 +22,11 @@ module EventService
 private
 
   # surely a better way
-  def self.game_url(game)
+  def self.url(game)
     "http://#{ActionMailer::Base.default_url_options[:host]}/games/#{game.id}"
+  end
+
+  def self.sequence(game)
+    game.updated_at.to_i - game.created_at.to_i
   end
 end
