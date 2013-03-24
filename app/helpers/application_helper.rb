b6d09d05ab8ccb585ae9438222028a8c8e74104d
Brandan Lennox <brandan@bclennox.com>
2013-03-24 11:11:24 -0400

    Moving stuff into ApplicationHelper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 module ApplicationHelper
   def admin?
     current_user.present?
@@ -6,5 +7,25 @@ module ApplicationHelper
   def relative_date(date)
     modifier = date < Time.now ? 'ago' : 'from now'
     time_ago_in_words(date) + ' ' + modifier
+  end
+
+  def title(t)
+    content_for(:title, t.to_s)
+  end
+
+  def page_title
+    if content_for?(:title)
+      "#{content_for(:title)} — #{app_name}"
+    else
+      app_name
+    end
+  end
+
+  def app_name
+    'Foxy Lady Ultimate'
+  end
+
+  def body_class
+    [controller.controller_name, controller.action_name].join(' ')
   end
 end
