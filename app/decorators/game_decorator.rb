b59dd132cb2729ac056ddb0ed7c8f3fba308d07a
Brandan Lennox <brandan@bclennox.com>
2013-05-25 10:22:48 -0400

    Moving relative_date into decorators.

--- app/decorators/game_decorator.rb
+++ app/decorators/game_decorator.rb
@@ -5,6 +5,10 @@ class GameDecorator < Draper::Decorator
     source.starts_at.strftime('%A, %B %-d, %Y at %l:%M%P')
   end
 
+  def relative_date
+    _relative_date(source.starts_at)
+  end
+
   def players(filter = nil)
     method = filter.nil? ? :players : filter ? :confirmed_players : :declined_players
     players = source.send(method).map(&:short_name)
@@ -14,5 +18,12 @@ class GameDecorator < Draper::Decorator
       when 1..2 then players.join(' and ')
       else           players[0..-2].join(', ') + ', and ' + players.last
     end
+  end
+
+private
+
+  def _relative_date(date)
+    modifier = date < Time.now ? 'ago' : 'from now'
+    h.time_ago_in_words(date) + ' ' + modifier
   end
 end
--- app/decorators/player_decorator.rb
+++ app/decorators/player_decorator.rb
@@ -8,9 +8,16 @@ class PlayerDecorator < Draper::Decorator
     tip = if games.empty?
       'Never played'
     else
-      "Last played #{h.relative_date(games.first.starts_at)}, #{h.pluralize(games.size, 'game')} total"
+      "Last played #{_relative_date(games.first.starts_at)}, #{h.pluralize(games.size, 'game')} total"
     end
 
     h.content_tag(:i, nil, class: icon, title: tip)
+  end
+
+private
+
+  def _relative_date(date)
+    modifier = date < Time.now ? 'ago' : 'from now'
+    h.time_ago_in_words(date) + ' ' + modifier
   end
 end
