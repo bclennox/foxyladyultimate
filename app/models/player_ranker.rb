f8626e5417dcaae04dffa0f1b0d28d3d74e10b75
Brandan Lennox <brandan@bclennox.com>
2013-05-29 00:50:28 -0400

    PlayerRanker only looks at past games.

--- app/models/player_ranker.rb
+++ app/models/player_ranker.rb
@@ -6,6 +6,7 @@ class PlayerRanker
       .select('players.*, COUNT(responses.id) AS games_played')
       .where('responses.playing' => true)
       .where('games.canceled' => false)
+      .where('games.starts_at < NOW()')
       .group('players.id')
       .reorder('games_played DESC')
   end
