deb377f23c5541f5235b8cf6a13878fc9bebf537
Brandan Lennox <brandan@bclennox.com>
2013-06-30 23:32:55 -0400

    Adding tests for PlayerDecorator.

---
+++ spec/decorators/player_decorator_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe PlayerDecorator do
+  describe '#attendance' do
+    let(:player) { FactoryGirl.create(:player) }
+    subject { player.decorate }
+
+    context 'when the player has never played' do
+      its(:attendance) { should == 'Never played' }
+    end
+
+    context 'when the player has played a few games' do
+      let(:games) { FactoryGirl.create_list(:game, 3) }
+      let(:last_game) { games.sort(&:started_at).last }
+
+      before do
+        games.each { |game| game.respond(player, true) }
+      end
+
+      its(:attendance) { should include('3 games') }
+    end
+  end
+end
