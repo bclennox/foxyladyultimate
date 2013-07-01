068fe5eee08adb97835fac310da03a0066a4e9f7
Brandan Lennox <brandan@bclennox.com>
2013-07-01 00:36:58 -0400

    Adding tests for games routing.

---
+++ spec/routing/games_routing_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe 'routes for games' do
+  it 'routes /games/schedule' do
+    expect(get('/games/schedule')).to route_to('games#schedule')
+  end
+
+  it 'routes /games/1/respond' do
+    expect(get('/games/1/respond')).to route_to('games#respond', id: '1')
+  end
+
+  it 'routes /games/1/remind' do
+    expect(post('/games/1/remind')).to route_to('games#remind', id: '1')
+  end
+
+  it 'routes /games/1/cancel' do
+    expect(post('/games/1/cancel')).to route_to('games#cancel', id: '1')
+  end
+
+  it 'routes /games/1/reschedule' do
+    expect(post('/games/1/reschedule')).to route_to('games#reschedule', id: '1')
+  end
+
+  it 'routes the root URL' do
+    expect(get('/')).to route_to('games#next')
+  end
+end
