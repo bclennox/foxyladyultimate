a8ca3a83294bcbb01fd6a0673c6f3398e2e98843
Brandan Lennox <brandan@bclennox.com>
2014-03-26 21:42:02 -0400

    Validating password confirmation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :recoverable, :timeoutable
 
+  validates :password, confirmation: true
+
   def name
     "#{first_name} #{last_name}"
   end
