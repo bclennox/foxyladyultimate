c84eb0cc13b5748d8bed93f307df119e26195bb3
Brandan Lennox <brandan@bclennox.com>
2021-01-15 23:57:44 -0500

    Symlink node_modules and public/packs in Capistrano.

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ set :application, 'foxyladyultimate.com'
 set :repo_url, 'github-foxyladyultimate:bclennox/foxyladyultimate'
 
 append :linked_files, '.rbenv-vars'
-append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache'
+append :linked_dirs, 'log', 'node_modules', 'public/packs', 'tmp/pids', 'tmp/cache'
 
 set :rbenv_ruby, File.read('.ruby-version').strip
 
