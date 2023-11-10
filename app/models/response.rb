fc828ab6eefc2670318a18077eb008ce5f8a5cc2
Brandan Lennox <brandan@bclennox.com>
2023-11-10 07:29:42 -0500

    Optimize queries on games.

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,4 +1,7 @@
 class Response < ApplicationRecord
   belongs_to :player, touch: true
   belongs_to :game, touch: true
+
+  scope :confirmed, -> { where(playing: true) }
+  scope :declined, -> { where(playing: false) }
 end
