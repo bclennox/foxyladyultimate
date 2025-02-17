b2ee62dc8abc628fd2b5912a100386c4e93a9237
Brandan Lennox <brandan@bclennox.com>
2025-02-16 23:58:18 -0500

    Upgrade to Rails 8.0.

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ Bundler.require(*Rails.groups)
 
 module FoxyLadyUltimate
   class Application < Rails::Application
-    config.load_defaults 7.2
+    config.load_defaults 8.0
     config.time_zone = 'Eastern Time (US & Canada)'
     config.active_record.schema_format = :sql
     config.active_job.queue_adapter = :good_job
