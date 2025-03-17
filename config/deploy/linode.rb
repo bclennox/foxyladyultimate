485b166217b4a531ade11d36da4ec08d3ecc14a8
Brandan Lennox <brandan@bclennox.com>
2025-03-17 01:18:30 -0400

    Fix assets precompile on deploy.

--- config/deploy/linode.rb
+++ config/deploy/linode.rb
@@ -4,6 +4,17 @@ set :deploy_to, "/home/deploy/apps/#{fetch(:application)}"
 set :default_env, path: '~/.rbenv/shims:~/.rbenv/bin:$PATH'
 
 namespace :deploy do
+  namespace :dartsass do
+    desc 'Build dartsass assets'
+    task :build do
+      on roles(:app) do
+        within release_path do
+          execute :rake, 'dartsass:build'
+        end
+      end
+    end
+  end
+
   desc 'Restart Puma service'
   task :restart_web do
     on roles(:app) do
@@ -18,5 +29,7 @@ namespace :deploy do
     end
   end
 end
+
+before 'deploy:assets:precompile', 'deploy:dartsass:build'
 after 'deploy:symlink:release', 'deploy:restart_web'
 after 'deploy:symlink:release', 'deploy:restart_workers'
